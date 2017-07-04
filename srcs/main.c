ebc1e5bbf58303d06bc917a600d749c0b7e13312
Antonin GAVREL <angavrel@e3r11p17.42.fr>
2017-07-04 23:53:19 +0200

    [ADD] implemented Perlin algo

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,85 @@
 /*   By: gmonein <gmonein@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/09 15:59:27 by gmonein           #+#    #+#             */
-/*   Updated: 2017/07/04 17:04:11 by angavrel         ###   ########.fr       */
+/*   Updated: 2017/07/04 23:52:00 by angavrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf.h"
+
+
+static void	ft_swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/****************/
+void perlin_init(int *p, float *gx, float *gy, int size, unsigned seed) {
+    int i;
+    srand(seed);
+
+	i = -1;
+	while (++i < size)
+        p[i] = i;
+    /* Randomly swap values */
+	i = -1;
+    while (++i < size)
+	{
+        int j = rand() % size;
+		ft_swap(&p[i], &p[j]);
+    }
+    /* Precompute table of gradients */
+	i = -1;
+    while (++i < size)
+	{
+        gx[i] = rand() / (float)RAND_MAX;
+        gy[i] = rand() / (float)RAND_MAX;
+    }
+}
+
+static inline int perlin_grab_q(int *p, int x, int y)
+{
+    return (p[(y + p[x & 0xff]) & 0xff]); // now need to protect against seg fault by insuring in range of table
+}
+
+float perlin_get(int *p, float *gx, float *gy, int size, float x, float y) {
+    int qx0 = (int)floorf(x);
+    int qx1 = qx0 + 1;
+    int qy0 = (int)floorf(y);
+    int qy1 = qy0 + 1;
+
+    /* Grab the precomputed, random values from the integer plane
+     * surrounding this point.
+     */
+    int q00 = perlin_grab_q(p, qx0, qy0);
+    int q01 = perlin_grab_q(p, qx1, qy0);
+    int q10 = perlin_grab_q(p, qx0, qy1);
+    int q11 = perlin_grab_q(p, qx1, qy1);
+
+    float tx0 = x - floorf(x);
+    float tx1 = tx0 - 1;
+    float ty0 = y - floorf(y);
+    float ty1 = ty0 - 1;
+
+    float v00 = gx[q00]*tx0 + gy[q00]*ty0;
+    float v01 = gx[q01]*tx1 + gy[q01]*ty0;
+    float v10 = gx[q10]*tx0 + gy[q10]*ty1;
+    float v11 = gx[q11]*tx1 + gy[q11]*ty1;
+
+    float wx = (3 - 2*tx0)*tx0*tx0;
+    float v0 = v00 - wx*(v00 - v01);
+    float v1 = v10 - wx*(v10 - v11);
+    float wy = (3 - 2*ty0)*ty0*ty0;
+
+    return (v0 - wy * (v0 - v1));
+}
+
+
+/****************/
 
 void		free_for_all(t_env *env)
 {
@@ -34,22 +108,46 @@ static void	put_pix(int *pixels, int clr, int x, int y)
 	pixels[(int)(WIN_W) * y + x] = clr;
 }
 
+
 static void perlin(t_env *env)
 {
+    int tx = 2;
+    int ty = 1; // standard perlin values for tx and ty
+	float f;
+    static int initted = 0;
+    static float gx[256];
+    static float gy[256];
+    static int p[256];
+	int	color;
 	int		x;
 	int		y;
-	unsigned int col;
 
+	if (!initted) {
+        perlin_init(p, gx, gy, 256, 1);
+        initted = 1;
+    }
 	y = -1;
 	while (++y < WIN_H)
 	{
 		x = -1;
 		while (++x < WIN_W)
 		{
-			col = ((int)((x / WIN_W) * 0xff0000)) & 0xff0000;
-			put_pix(env->pixels, 0x00FFFF | col, x, y);
+			float x_field = (x + (tx / 2.0)) / 128;
+			float y_field = (y + (ty / 2.0)) / 128;
+	//		write(1, "a\n", 2);
+			f = perlin_get(p, gx, gy, 256, x_field, y_field);
+			f = sinf(f * 64) * 128 + 128;
+			color = (((int)f << 16) | ((int)f << 8) | (int)f);
+			//color = ((f * 0x10000) + (f * 0x100) + f);
+			put_pix(env->pixels, color, x, y);
+		//	pixels[y*screen->w+x] = px;
+
+
+		//	col = ((int)((x / WIN_W) * 0xff0000)) & 0xff0000;
+//			put_pix(env->pixels, 0x00FFFF | col, x, y);
 		}
 	}
+//	SDL_Flip(screen);
 }
 
 static int	global_loop(t_env *env)
@@ -112,8 +210,8 @@ int			init_sdl(t_env *env)
 	env->render = SDL_CreateRenderer(env->win, 0,
 			SDL_RENDERER_TARGETTEXTURE
 			| SDL_RENDERER_SOFTWARE
-			| SDL_RENDERER_ACCELERATED
-			| SDL_RENDERER_PRESENTVSYNC);
+			| SDL_RENDERER_ACCELERATED);
+		//	| SDL_RENDERER_PRESENTVSYNC);
 	SDL_SetRenderDrawColor(env->render, 0xC0, 0, 0, 255);
 	SDL_RenderClear(env->render);
 	SDL_RenderPresent(env->render);
